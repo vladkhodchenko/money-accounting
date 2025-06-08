ce10dd63648b59f2c1a980b66bcf64759696d0ea
Sidyuk Vlad <sidyukvlad@gmail.com>
2025-06-08 11:11:45 +0300

    Use linter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from enum import  Enum
+from enum import Enum
 
 from typing import List
 
@@ -8,12 +8,12 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 
-
 class Capitalization(str, Enum):
-    MONTHLY = "monthly" # ежемесячная - каждый месяц прибавляются проценты
-    DAY = "day"         # ежедневная - каждый день прибавляются проценты (как в яндексе)
-    END = "end"         # в конце срока прибавляются проценты (самое невыгодное)
-
+    MONTHLY = "monthly"  # ежемесячная - каждый месяц прибавляются проценты
+    # ежедневная - каждый день прибавляются проценты (как в яндексе)
+    DAY = "day"
+    # в конце срока прибавляются проценты (самое невыгодное)
+    END = "end"
 
 
 @dataclass
@@ -28,7 +28,7 @@ class Deposit:
     @property
     def date_to(self):
         return self.date_from + relativedelta(months=self.term_months)
-        
+
     def calculate_profit_in_month(self) -> float:
         profit = self.initial_amount * (self.interest_rate / 100 / 12)
         return round(profit, 2)
@@ -45,54 +45,52 @@ class Deposit:
 class Portfolio:
     def __init__(self):
         self.deposits: List[Deposit] = []
-    
+
     def add_deposit(self, *deposits: Deposit):
         for deposit in deposits:
             self.deposits.append(deposit)
-    
+
     def get_deposits(self):
         list_name_deposits = []
         for deposit in self.deposits:
             list_name_deposits.append(deposit.name)
         return list_name_deposits
-        
 
     def calculate_total(self, target_months: int) -> float:
         """Рассчитывает общую сумму через заданное количество месяцев"""
         total = 0.0
-        
+
         for deposit in self.deposits:
-            total += deposit.initial_amount + deposit.calculate_profit_in_month() * target_months
-        
+            total += deposit.initial_amount + deposit.calculate_profit_in_month() * \
+                target_months
+
         return round(total, 2)
-    
+
     def forecast(self, periods: List[int]) -> dict:
         """Возвращает прогноз для списка периодов (в месяцах)"""
         return {months: self.calculate_total(months) for months in periods}
-
 
 
 # Пример использования
 if __name__ == "__main__":
     # Создаем портфель сбережений
     portfolio = Portfolio()
-    
 
     deposit_tinkof = Deposit(
-        name = "deposit_tinkof",
+        name="deposit_tinkof",
         initial_amount=10000,
         interest_rate=19.0,
         term_months=3,
-        date_from = date.today(),
+        date_from=date.today(),
         capitalization='monthly'
     )
 
     deposit_alfa = Deposit(
-        name = "deposit_alfa",
+        name="deposit_alfa",
         initial_amount=100000,
         interest_rate=20.03,
         term_months=3,
-        date_from = date.today(),
+        date_from=date.today(),
         capitalization='monthly'
     )
 
@@ -107,4 +105,4 @@ if __name__ == "__main__":
     # Делаем прогноз на разные периоды
     forecast = portfolio.forecast([3, 6, 12])
 
-    print(f"forecast=", forecast)
+    print("forecast=", forecast)
