d1b09d2f9d902e8e61231fa8647a49dc170ad410
Sidyuk Vlad <sidyukvlad@gmail.com>
2025-07-13 17:31:45 +0300

    Add base jwt auth

--- app/auth/router.py
+++ app/auth/router.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import HTTPBasic, HTTPBasicCredentials, OAuth2PasswordBearer
 
 from auth.service import UserService
 from passlib.context import CryptContext
 from auth.schemas import User, UserBase, UserInDB
 import secrets
 import jwt
+import auth.security 
 
 router = APIRouter(prefix="/who", tags=["Auth"])
 
@@ -14,15 +15,16 @@ secret_password: str = "password"
 basic: HTTPBasicCredentials = HTTPBasic()
 
 myctx = CryptContext(schemes=["sha256_crypt", "des_crypt"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-fake_db = [UserInDB(name='user2', hashed_password='$5$rounds=535000$PGFQ/exkYHEEoTrq$YtFhsWSVPyf72bbtpHR3FXioMtQFPoVgbRatfrhifn.')]
+
+fake_db = [UserInDB(name='user2', hashed_password='$5$rounds=535000$fzF/wlaG/XuHFr2p$gvAFxA2n1CDRqkW6JmjWDELVEElRRBgViHi1H48qK7C')]  #password = "password"
 
 
 @router.post("/register")
 def register(register_data: User) -> UserBase:
     pwd = register_data.password
     hashed_password = myctx.hash(register_data.password)
-    
     user_in_db = UserInDB(
         name = register_data.name,
         hashed_password = hashed_password,
@@ -41,6 +43,7 @@ def register(register_data: User) -> UserBase:
 
 @router.post("/login")
 def login(user_data: User):
+    print(fake_db)
     is_exist = is_exist_user(fake_db, user_data.name)
     if is_exist:
         if myctx.verify(user_data.password, is_exist.hashed_password):
@@ -56,9 +59,10 @@ def login(user_data: User):
     
 def is_exist_user(db, name):
     for user in db:
+        print(user)
         if user.name == name:
             return user
-        return False
+        return None
 
 
 # Basic auth
@@ -80,3 +84,35 @@ def logout():
             detail="You have successfully logged out",
             headers={"WWW-Authenticate": "Basic"},
         )
+
+
+@router.post("/login_jwt")
+def login_jwt(user_data: User):
+    is_exist = is_exist_user(fake_db, user_data.name)
+    print(is_exist)
+    if is_exist:
+        if myctx.verify(user_data.password, is_exist.hashed_password):
+            token = auth.security.create_jwt_token({"sub": user_data.name})
+            return {"token":token}
+        else:
+            raise HTTPException(
+            status_code=401, detail="Incorrect password"
+        )
+    else:
+        raise HTTPException(
+            status_code=401, detail="Incorrect username or password"
+        )
+    
+
+@router.get("/protected_resource")
+def protect_resource(token: str = Depends(oauth2_scheme)):
+    # user = is_exist_user(fake_db, current_user)
+    print(token)
+    # try:
+    #     payload = jwt.decode(token, auth.security.SECRET_KEY, algorithms=[auth.security.ALGORITHM])
+    #     print(payload)
+    # except jwt.ExpiredSignatureError:
+    #     raise HTTPException(status_code=401, detail="Token has expired", headers={"WWW-Authenticate": "Bearer"})
+    # except jwt.DecodeError:
+    #     raise HTTPException(status_code=401, detail="Invalid token", headers={"WWW-Authenticate": "Bearer"})
+    # return user
---
+++ app/auth/security.py
@@ -0,0 +1,43 @@
+# security.py
+import jwt
+import datetime
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from typing import Dict
+
+# OAuth2PasswordBearer извлекает токен из заголовка "Authorization: Bearer <token>"
+# Параметр tokenUrl указывает маршрут, по которому клиенты смогут получить токен
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+SECRET_KEY = "mysecretkey"  # В реальной практике генерируйте ключ, например, с помощью 'openssl rand -hex 32', и храните его в безопасности
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 15  # Время жизни токена
+
+# Функция для создания JWT токена с заданным временем жизни
+def create_jwt_token(data: Dict):
+    """
+    Функция для создания JWT токена. Мы копируем входные данные, добавляем время истечения и кодируем токен.
+    """
+    to_encode = data.copy()  # Копируем данные, чтобы не изменить исходный словарь
+    expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)  # Задаем время истечения токена
+    to_encode.update({"exp": expire})  # Добавляем время истечения в данные токена
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)  # Кодируем токен с использованием секретного ключа и алгоритма
+
+# Функция для получения пользователя из токена
+def get_user_from_token(token: str = Depends(oauth2_scheme)):
+    """
+    Функция для извлечения информации о пользователе из токена. Проверяем токен и извлекаем утверждение о пользователе.
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])  # Декодируем токен с помощью секретного ключа
+        return payload.get("sub")  # Возвращаем утверждение о пользователе (subject) из полезной нагрузки
+    except jwt.ExpiredSignatureError:
+        pass  # Обработка ошибки истечения срока действия токена
+    except jwt.InvalidTokenError:
+        pass  # Обработка ошибки недействительного токена
+
+
+if __name__ == "__main__":
+    print("hello_world")
+    token = create_jwt_token({"sub": "user"})
+    print({"access_token": token, "token_type": "bearer"})
