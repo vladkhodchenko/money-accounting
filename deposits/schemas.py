f311670ffe0bc0ce5ad8ff39bf2df829bf5a166f
hodchenko <sidyukvlad@gmail.com>
2026-01-11 21:07:03 +0300

    Update deposits/schemas

--- deposits/schemas.py
+++ deposits/schemas.py
@@ -1,25 +1,51 @@
-from pydantic import BaseModel, Field, validator
-from typing import Union, List, Optional
-from datetime import (
-    date,
-    datetime,
-)
-
+from pydantic import BaseModel, Field, validator, UUID4
+from datetime import date
+from typing import Optional
 from deposits.consts import Capitalization, TypeBankAccount
 
 
-class SDepositCreate(BaseModel):
+class DepositSchema(BaseModel):
+    id: UUID4
+    name: str
+    name_bank: str
+
+    initial_amount: float
+    interest_rate: float
+    term_months: int
+
+    date_from: date | None
+    date_to: date
+
+    capitalization_id: Capitalization = Capitalization.MONTHLY
+    deposit_type_id: TypeBankAccount = TypeBankAccount.DEPOSIT
+
+    earned_amount: float
+    final_amount: float
+
+
+class CreateDepositRequestSchema(BaseModel):
     name: str
     name_bank: str = "tinkoff"
+
     initial_amount: float = 10000.0
     interest_rate: float = 20.0
     term_months: int = 6
-    date_from: Union[date, None] = None
+
+    date_from: date | None = None
+
     capitalization_id: Capitalization = Capitalization.MONTHLY
     deposit_type_id: TypeBankAccount = TypeBankAccount.DEPOSIT
 
 
-class SDeposit(SDepositCreate):
+class CreateDepositResponseSchema(BaseModel):
+    deposit: DepositSchema
+
+    # class Config:
+    #     orm_mode = True
+    #     exclude = {"created_at", "updated_at"}
+
+
+class GetDepositResponseSchema(DepositSchema):
     date_to: date
     earned_amount: float
     final_amount: float
@@ -29,18 +55,24 @@ class SDeposit(SDepositCreate):
         exclude = {"created_at", "updated_at"}
 
 
-class SDepositPatch(BaseModel):
-    name: Optional[str] = None
-    name_bank: Optional[str] = None
-    initial_amount: Optional[float] = Field(None, gt=0)
-    interest_rate: Optional[float] = Field(None, gt=0)
-    term_months: Optional[int] = Field(None, gt=0)
-    date_from: Optional[date] = None
-    capitalization_id: Optional[Capitalization] = Capitalization.MONTHLY
-    deposit_type_id: Optional[TypeBankAccount] = TypeBankAccount.DEPOSIT
+class GetDepositsQuerySchema(DepositSchema):
+    deposit_id: str
 
-    @validator("initial_amount", "interest_rate", "term_months", pre=True)
-    def validate_positive_values(cls, v):
-        if v is not None and v <= 0:
-            raise ValueError("Value must be greater than 0")
-        return v
+
+class GetDepositsResponseSchema(DepositSchema):
+    deposits: list[DepositSchema]
+
+
+class UpdateDepositRequestSchema(BaseModel):
+    name: str | None = None
+    name_bank: str | None = None
+    initial_amount: float | None = Field(None, gt=0)
+    interest_rate: float | None = Field(None, gt=0)
+    term_months: int | None = Field(None, gt=0)
+    date_from: date | None = None
+    capitalization_id: Capitalization = Capitalization.MONTHLY
+    deposit_type_id: TypeBankAccount = TypeBankAccount.DEPOSIT
+
+
+class UpdateDepositResponseSchema(BaseModel):
+    deposit: DepositSchema
